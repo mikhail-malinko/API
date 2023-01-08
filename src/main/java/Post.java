b435fd845521f3784888d666c44eeaf19a439300
Mikhail156 <mikhail156@mail.ru>
2023-01-08 09:59:18 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phome;
+    public String surname;
+    public boolean subscription;
+}
